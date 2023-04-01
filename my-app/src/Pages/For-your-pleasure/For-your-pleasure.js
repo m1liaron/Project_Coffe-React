44674c532285caf036f943b7e218d6bfa07d1848
vlad <example@gmail.com>
2023-04-01 11:12:42 +0300

    coffee and for your pleasure

--- my-app/src/Pages/For-your-pleasure/For-your-pleasure.js
+++ my-app/src/Pages/For-your-pleasure/For-your-pleasure.js
@@ -1,12 +1,56 @@
 import React from 'react';
-import './For-your-pleasure.css';
+import './For-your-pleasure.scss';
+
+
+import  CardList  from "../../Components/CardList/CardList";
+import {data} from "../../resources/data/dataFilter";
+import Nav from '../../Components/Nav/Nav';
+import Footer from '../../Components/Footer/Fotter';
+
+import AboutCoffeeBens from '../../resources/images/coffee-beans-about.svg';
+import ImgCoffee from '../../resources/For-your-pleasure/Coffee.png';
 
 function Pleasure() {
-    
   return (
-    <div>   
-        <h1>It'S Pleasure PAGE</h1>
-    </div>
+    <>   
+        <div className="header">
+        <div className="container-text">
+          <div className="col">
+            <div className="row">
+                <Nav/>
+                <h1 className='main-title'>For your pleasure</h1>
+            </div>
+          </div>
+        </div>
+        </div>
+        <div className="about">
+        <div className="container-content">
+        <div className="flex-about">
+              <div className="image_about">
+                <img src={ImgCoffee} alt="" />
+              </div>
+              <div className="about_coffee">
+                  <h1 className='title-about-coffee'>About our goods</h1>
+                  <div className="img_coffee">
+                    <img src={AboutCoffeeBens} alt="" />
+                  </div>
+                    <p className='paragrapg-about'>Extremity sweetness difficult behaviour he of. On disposal of as landlord horrible.</p>
+                    <p className='paragrapg-about'>
+                    Afraid at highly months do things on at. Situation recommend objection do intention so questions.
+                    As greatly removed calling pleased improve an. Last ask him cold feel met spot shy want. Children me laughing we prospect answered followed. At it wentis song that held help face. 
+                    </p>
+              </div>
+            </div>
+            <div className="line"></div>
+        </div>
+        </div>
+        <div className="cards-coffee">
+        <div className="list-cards">
+            <CardList data={data} /> 
+          </div>
+        </div>
+        <Footer/>
+    </>
   );
 }
 
--- my-app/src/Pages/Our-coffee/Coffee.js
+++ my-app/src/Pages/Our-coffee/Coffee.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './Coffee.scss';
 
-import Footer from '../../Components/Footer/Fotter';
 import Nav from '../../Components/Nav/Nav';
 
+import AboutCoffeeBens from '../../resources/images/coffee-beans-about.svg';
+
 import ImgWoman from '../../resources/Our-Coffee-img/WomanWithCupOfCoffee.png';
+
+import  CardList  from "../../Components/CardList/CardList";
+import {data} from "../../resources/data/dataFilter";
+import Footer from '../../Components/Footer/Fotter';
 
 function Coffee() {
   return (
@@ -21,11 +26,47 @@ function Coffee() {
       </div>
       <div className="about">
         <div className="container-content">
-          <img src={ImgWoman} alt="" />
+            <div className="flex-about">
+              <div className="image_about">
+                <img src={ImgWoman} alt="" />
+              </div>
+              <div className="about_coffee">
+                  <h1 className='title-about-coffee'>About our beans</h1>
+                  <div className="img_coffee">
+                    <img src={AboutCoffeeBens} alt="" />
+                  </div>
+                    <p className='paragrapg-about'>Extremity sweetness difficult behaviour he of. On disposal of as landlord horrible.</p>
+                    <p className='paragrapg-about'>
+                    Afraid at highly months do things on at. Situation recommend objection do intention so questions. 
+                    As greatly removed calling pleased improve an. Last ask him cold feel met spot shy want. 
+                    Children me laughing we prospect answered followed. At it went is song that held help face.
+                    </p>
+              </div>
+            </div>
+            <div className="line"></div>
         </div>
       </div>
-      <div className="filter__coffee"></div>
-      {/* <Footer/> */}
+      <div className="filter__coffee">
+        <div className="cards-container">
+          <div className="container-interaction">
+            <div className="container-search">
+              <h1 className='search-title'>Lookiing for</h1>
+              <input className='input-search' type="text" 
+              placeholder='start typing here...' />
+            </div>
+            <div className="container-btn-filter">
+              <h1 className='filter-title'>Or filter</h1>
+                    <button className='btn-filter'>Brazil</button>
+                    <button className='btn-filter'>Kenya</button>
+                    <button className='btn-filter'>Kenya</button>
+            </div>
+          </div>
+          <div className="list-cards">
+            <CardList data={data} /> 
+          </div>
+        </div>
+      </div>
+      <Footer/>
     </>
 
 
